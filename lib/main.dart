58a933da1d4f5cbaa78f3e8183669141a7f95719
FlutLab.io <>
2021-06-03 11:57:53 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,8 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        debugShowCheckedModeBanner: false,
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
